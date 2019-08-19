1133187651f6970fde53a023ba18f06eb8a39947
JJongSue <jspark940321@gmail.com>
2019-08-19 23:47:30 +0900

    190819

--- 16988/16988.cpp
+++ 16988/16988.cpp
@@ -1,31 +1,91 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-static int N, M, ans = 0;
-vector<vector<int>> map;
+static int N, M, ans = 0, cnt;
+static vector<vector<int>> map;
+static vector<vector<int>> copy_map;
+static bool is_count;
+bool is_map(int x, int y);
+void count_bd(int x, int y);
 int main(void)
 {
     //행렬 N, M
     //0빈칸 1나의돌 2상대돌
     //돌 2개로 죽일 수 있는 상대 돌의 최대 갯수
     scanf("%d %d", &N, &M);
-    map.assign(N+2,vector<int>(M+2,1));
+    map.assign(N+1,vector<int>(M+1,1));
     for(int i=0;i<N;i++)
     {
         for(int j=0;j<M;j++)
         {
-            scanf(" %d", &map[i+1][j+1]);
-        }
-    }
-    for(int i=1;i<N*M;i++)
-    {
-        if(map[i/N][i%M] != 0) continue;
-        for(int j=i+1;j<=N*M;j++)
-        {
-            if(map[j/N][j%M] != 0) break;
-            map[i/N][i%M] = 1;
-            map[j/N][j%M] = 1;
+            scanf(" %d", &map[i][j]);
         }
     }
 
+    for(int i=0;i<N*M;i++)
+    {
+        int s = M;//M>N ? M : N;
+        if(map[i/s][i%s] != 0) continue;
+        for(int j=i+1;j<N*M;j++)
+        {
+            if(map[j/s][j%s] != 0) continue;
+            copy_map.clear();            
+            copy_map=map;
+            copy_map[i/s][i%s] = 1;
+            copy_map[j/s][j%s] = 1;
+            int anstmp=0;
+            /*for(int cpmi=0;cpmi<N;cpmi++)
+            {
+                for(int cpmj=0;cpmj<M;cpmj++)
+                {
+                    printf("%d ", copy_map[cpmi][cpmj]);
+                }printf("\n");
+            }printf("\n\n");*/
+
+            for(int fi=0;fi<N;fi++)
+            {
+                for(int fj=0;fj<M;fj++)
+                {
+                    if(copy_map[fi][fj]==2)
+                    {
+                        is_count=true;
+                        cnt=0;
+                        count_bd(fj,fi);
+                        if(is_count)
+                        {
+                            anstmp+=cnt;
+                        }
+                    }
+                }
+            }//if(anstmp!=0) printf("%d", anstmp);
+            if(ans < anstmp) {ans=anstmp; }
+        }
+    }
+    printf("%d\n", ans);
+    return 0;
+}
+
+void count_bd(int x, int y)
+{
+    if(!is_map(x,y)) return;
+    if(copy_map[y][x] == 2){
+        copy_map[y][x] = -1;
+        cnt++;
+        count_bd(x+1,y);
+        count_bd(x-1,y);
+        count_bd(x,y+1);
+        count_bd(x,y-1);
+        return;
+    }
+    if(copy_map[y][x] == 0)
+    {
+        is_count=false;
+    }
+    return;
+
+}
+bool is_map(int x, int y)
+{
+    if(x < 0 || y < 0 || x >= M || y>=N) return false;
+    return true;
 }
